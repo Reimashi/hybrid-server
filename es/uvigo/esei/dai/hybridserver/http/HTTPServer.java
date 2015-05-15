53636f1424d0d150151220e3fd211ae8d6b4af42
Aitor González Fernández <reimashi@gmail.com>
2015-05-15 22:02:04 +0200

    Minor fix

--- es/uvigo/esei/dai/hybridserver/http/HTTPServer.java
+++ es/uvigo/esei/dai/hybridserver/http/HTTPServer.java
@@ -9,8 +9,6 @@ import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import es.uvigo.esei.dai.hybridserver.HybridServer;
-
 public class HTTPServer implements Runnable {
     private static final Logger log = Logger.getLogger(HTTPServer.class.getName());
     
