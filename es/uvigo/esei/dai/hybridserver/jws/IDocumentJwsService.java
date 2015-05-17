a6f6c4646a84c9fb7def06e3c6565cfc8e8eb843
Aitor González Fernández <reimashi@gmail.com>
2015-05-18 00:18:04 +0200

    Modificada interfaz SOAP para añadir mejor tipado

--- es/uvigo/esei/dai/hybridserver/jws/IDocumentJwsService.java
+++ es/uvigo/esei/dai/hybridserver/jws/IDocumentJwsService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.UUID;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 import es.uvigo.esei.dai.hybridserver.document.DocumentBean;
@@ -12,15 +14,19 @@ import es.uvigo.esei.dai.hybridserver.document.DocumentBeanType;
 
 @WebService
 public interface IDocumentJwsService {
-	@WebMethod
-	public DocumentBean get(DocumentBeanType type, UUID id);
+	@WebMethod(operationName = "getDocument")
+	@WebResult(name = "document")
+	public DocumentBean get(@WebParam(name = "docType") DocumentBeanType type, @WebParam(name = "docId") UUID id);
 
-    @WebMethod
-    public boolean delete(DocumentBeanType type, UUID id);
+	@WebMethod(operationName = "deleteDocument")
+    @WebResult(name = "isDeleted")
+    public boolean delete(@WebParam(name = "docType") DocumentBeanType type, @WebParam(name = "docId") UUID id);
 
-    @WebMethod
-    public List<DocumentBeanInfo> list(DocumentBeanType type);
+    @WebMethod(operationName = "listDocuments")
+    @WebResult(name = "listDocuments")
+    public List<DocumentBeanInfo> list(@WebParam(name = "docType") DocumentBeanType type);
 
-    @WebMethod
-    public int count(DocumentBeanType type);
+    @WebMethod(operationName = "countDocuments")
+    @WebResult(name = "numDocuments")
+    public int count(@WebParam(name = "docType") DocumentBeanType type);
 }
